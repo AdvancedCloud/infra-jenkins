2bc6c6be86dffdb218c882f44a0fa28c0a3ca2bd
Yash Bhatia <113397515+bhatiayash@users.noreply.github.com>
2024-06-05 01:17:17 -0400

    infrastructure code to host Jenkins (#1)

---
+++ ec2.tf
@@ -0,0 +1,47 @@
+# Creating key pair to enable ssh connection to ec2 instance
+
+data "aws_key_pair" "ec2" {
+  key_name = "ec2"
+}
+
+
+# Creating  EC2 Instance
+resource "aws_instance" "jenkins_instance" {
+  ami                     = data.aws_ami.jenkins-ami.id
+  instance_type           = var.configuration.ec2_instance.instance_type
+  disable_api_termination = "false"
+  root_block_device {
+    volume_size           = var.configuration.ec2_instance.volume_size
+    volume_type           = var.configuration.ec2_instance.volume_type
+    delete_on_termination = true
+  }
+  subnet_id                   = aws_subnet.public-subnets.id
+  security_groups             = ["${aws_security_group.jenkins_security_group.id}"]
+  key_name                    = data.aws_key_pair.ec2.key_name
+  associate_public_ip_address = true
+  tags = {
+    "Name" = "Jenkins Server"
+  }
+
+  user_data_base64 = base64encode(join("\n", [
+    "#!/bin/bash",
+    "sudo sed -i 's/:80 {/jenkins.${var.host_name} {/g' /etc/caddy/Caddyfile",
+    "sudo sed -i '/reverse_proxy localhost:8080/a\\ \\ \\ \\ tls ${var.email}' /etc/caddy/Caddyfile",
+    "sudo systemctl restart caddy",
+    "sudo fallocate -l 2G /swapfile",
+    "sudo chmod 600 /swapfile",
+    "sudo mkswap /swapfile",
+    "sudo swapon /swapfile",
+    "sudo echo '/swapfile swap swap defaults 0 0' >> /etc/fstab",
+    "echo 'jenkins ALL=(ALL:ALL) NOPASSWD:ALL' | sudo tee -a /etc/sudoers",
+    "sudo systemctl restart jenkins"
+  ]))
+
+}
+
+# Attaching Elastic IP to EC2
+resource "aws_eip_association" "eip_association" {
+  instance_id   = aws_instance.jenkins_instance.id
+  allocation_id = data.aws_eip.elastic_ip.id
+}
+
---
+++ main.tf
@@ -0,0 +1,50 @@
+# Creating a VPC
+resource "aws_vpc" "main" {
+  cidr_block       = var.vpc_cidr
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "Jenkins VPC"
+  }
+}
+
+#Creating public subnet
+resource "aws_subnet" "public-subnets" {
+  vpc_id                  = aws_vpc.main.id
+  availability_zone       = element(data.aws_availability_zones.available_zones.names, 0)
+  cidr_block              = cidrsubnet(var.vpc_cidr, 8, 1)
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "Jenkins Public Subnet"
+  }
+}
+
+# creating Internet Gateway for VPC
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "Jenkins Internet Gateway"
+  }
+}
+
+# Route Table for Public Subnet
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = var.publicroutetablecidr
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "Jenkins Public Route Table"
+  }
+}
+
+# Association between Public Subnet 1 and Public Route Table
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public-subnets.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
