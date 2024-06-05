2c817b34b17f4c092ea7605ba5dc26a8907b4b38
Yash Bhatia <113397515+bhatiayash@users.noreply.github.com>
2024-06-05 01:16:43 -0400

    infrastructure code to host Jenkins

---
+++ variables.tf
@@ -0,0 +1,68 @@
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+data "aws_availability_zones" "available_zones" {
+  state = "available"
+}
+
+variable "publicroutetablecidr" {
+  default = "0.0.0.0/0"
+}
+
+data "aws_ami" "jenkins-ami" {
+  most_recent = true
+  owners      = ["757569451969"]
+
+  filter {
+    name   = "name"
+    values = ["JenkinsAMI_*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+variable "configuration" {
+  description = "Configuration Settings"
+  type        = map(any)
+  default = {
+    "ec2_instance" = {
+      instance_type = "t2.micro"
+      volume_size   = 50
+      volume_type   = "gp2"
+    }
+  }
+}
+
+variable "host_name" {
+  type    = string
+  default = "bhatiayash.me"
+}
+
+variable "email" {
+  type    = string
+  default = "ybhatia500@gmail.com"
+}
+
+data "aws_route53_zone" "zone_name" {
+  name = var.host_name
+}
+
+data "aws_eip" "elastic_ip" {
+  filter {
+    name   = "tag:Name"
+    values = ["JenkinsElasticIP"]
+  }
+}
